6bd09554698bf1d7acaf5630d87177a61952093b
Ryan Weaver <ryan@thatsquality.com>
2019-05-02 13:34:45 -0400

    adding homepage

---
+++ src/Controller/MainController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return $this->render('main/homepage.html.twig');
+    }
+}
