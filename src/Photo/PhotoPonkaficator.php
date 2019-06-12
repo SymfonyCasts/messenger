031aa34d81e6c074d1fb96671b464968d214d912
Ryan Weaver <ryan@thatsquality.com>
2019-06-12 14:12:54 -0400

    constraining the photo intelligently

--- src/Photo/PhotoPonkaficator.php
+++ src/Photo/PhotoPonkaficator.php
@@ -4,6 +4,7 @@ namespace App\Photo;
 
 use App\Entity\ImagePost;
 use Doctrine\ORM\EntityManagerInterface;
+use Intervention\Image\Constraint;
 use Intervention\Image\ImageManager;
 use League\Flysystem\FilesystemInterface;
 
@@ -22,12 +23,23 @@ class PhotoPonkaficator
 
     public function ponkafy(ImagePost $imagePost)
     {
-        $image = $this->imageManager->make(
+        $targetPhoto = $this->imageManager->make(
             $this->photoFilesystem->readStream($imagePost->getFilename())
         );
 
-        $image = $image->insert(
-            file_get_contents(__DIR__.'/../../assets/ponka/ponka1.jpg'),
+        $ponkaFilename = __DIR__.'/../../assets/ponka/ponka1.jpg';
+        $ponkaPhoto = $this->imageManager->make($ponkaFilename);
+
+        $targetWidth = $targetPhoto->width() * .2;
+        $targetHeight = $targetPhoto->height() * .4;
+
+        $ponkaPhoto->resize($targetWidth, $targetHeight, function(Constraint $constraint) {
+            $constraint->aspectRatio();
+            $constraint->upsize();
+        });
+
+        $targetPhoto = $targetPhoto->insert(
+            $ponkaPhoto,
             'bottom-left',
             50,
             50
@@ -35,7 +47,7 @@ class PhotoPonkaficator
 
         $this->photoFilesystem->update(
             $imagePost->getFilename(),
-            $image->encode()
+            $targetPhoto->encode()
         );
 
         $imagePost->markAsPonkaAdded();
